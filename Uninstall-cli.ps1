a3bc2e796b5d43f1839ea05fbd5a2d89926c0018
keteague <kteague+github.com@pobox.com>
2022-02-12 15:55:42 -0600

    Add files via upload

---
+++ Uninstall-cli.ps1
@@ -0,0 +1,17 @@
+﻿$search = "Unitrends Agent*"
+
+
+$installed = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |  Select-Object DisplayName, UninstallString
+$installed += Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, UninstallString
+#$installed | ?{ $_.DisplayName -ne $null } | sort-object -Property DisplayName -Unique | Format-Table -AutoSize
+
+
+$result =$installed | ?{ $_.DisplayName -ne $null } | Where-Object {$_.DisplayName -like $search } 
+
+
+if ($result.uninstallstring -like "msiexec*") {
+    $args=(($result.UninstallString -split ' ')[1] -replace '/I','/X ') + ' /q'
+    Start-Process msiexec.exe -ArgumentList $args -Wait
+} else {
+    Start-Process $result.UninstallString -Wait
+}
